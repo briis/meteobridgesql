9d1d75f5edf416f9c1756aa0aa0b1788d134b803
Bjarne Riis <bjarne@briis.com>
2024-01-22 16:42:07 +0000

    Initial Setup and Dev environment

---
+++ custom_components/meteobridgesql/config_flow.py
@@ -0,0 +1,101 @@
+"""Config flow to configure WeatherFlow Forecast component."""
+from __future__ import annotations
+
+import logging
+import voluptuous as vol
+from typing import Any
+from homeassistant import config_entries
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_MAC,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_USERNAME,
+)
+from homeassistant.core import callback
+from homeassistant.data_entry_flow import FlowResult
+from pymeteobridgesql import MeteobridgeSQL
+from .const import (CONF_DATABASE, DEFAULT_PORT, DOMAIN)
+
+_LOGGER = logging.getLogger(__name__)
+
+class WeatherFlowForecastHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config Flow for WeatherFlow Forecast."""
+
+    VERSION = 1
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> config_entries.OptionsFlow:
+        """Get the options flow for WeatherFlow Forecast."""
+        return WeatherFlowForecastOptionsFlowHandler(config_entry)
+
+    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
+        """Handle a flow initialized by the user."""
+
+        if user_input is None:
+            return await self._show_setup_form(user_input)
+
+        errors = {}
+
+        meteobridge = MeteobridgeSQL(host=user_input[CONF_HOST],user=user_input[CONF_USERNAME],password=user_input[CONF_PASSWORD], database=user_input[CONF_DATABASE])
+        meteobridge.async_init()
+
+        await self.async_set_unique_id(user_input[CONF_MAC])
+        self._abort_if_unique_id_configured
+
+        return self.async_create_entry(
+            title=f"Meteobride SQL ({user_input[CONF_MAC]})",
+            data={
+                CONF_MAC: user_input[CONF_MAC],
+                CONF_HOST: user_input[CONF_HOST],
+                CONF_PORT: user_input[CONF_PORT],
+                CONF_USERNAME: user_input[CONF_USERNAME],
+                CONF_PASSWORD: user_input[CONF_PASSWORD],
+                CONF_DATABASE: user_input[CONF_DATABASE],
+            }
+        )
+
+    async def _show_setup_form(self, errors=None):
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_MAC): str,
+                    vol.Required(CONF_HOST): str,
+                    vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Required(CONF_DATABASE): str,
+                }
+            ),
+            errors=errors or {},
+        )
+
+class WeatherFlowForecastOptionsFlowHandler(config_entries.OptionsFlow):
+    """Options Flow for WeatherFlow Forecast component."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize the WeatherFlow Forecast Options Flows."""
+        self._config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
+        """Configure Options for WeatherFlow Forecast."""
+
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_MAC, default=self._config_entry.data.get(CONF_MAC, "")): str,
+                    vol.Required(CONF_HOST, default=self._config_entry.data.get(CONF_HOST, "")): str,
+                    vol.Required(CONF_PORT, default=self._config_entry.data.get(CONF_PORT, DEFAULT_PORT)): int,
+                    vol.Required(CONF_USERNAME, default=self._config_entry.data.get(CONF_USERNAME, "")): str,
+                    vol.Required(CONF_PASSWORD, default=self._config_entry.data.get(CONF_PASSWORD, "")): str,
+                    vol.Required(CONF_DATABASE, default=self._config_entry.data.get(CONF_DATABASE, "")): str,
+                }
+            )
+        )
---
+++ custom_components/meteobridgesql/const.py
@@ -0,0 +1,6 @@
+"""Constants for Meteobridge SQL component."""
+
+CONF_DATABASE = "database"
+
+DEFAULT_PORT = 3306
+DOMAIN = "meteobridgesql"
